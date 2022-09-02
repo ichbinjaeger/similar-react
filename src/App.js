026bb28abbfee04e4eee0efe7b1d47fe60079380
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2022-09-02 17:27:02 +0200

    fetch data, log movies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
+import getPage from "./api/content"
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+  const [movieData, setMovieData] = useState(null)
+
+  const fetchData = async () => {
+    const res = await getPage()
+    console.log(res)
+    setMovieData(res.blocks[0].products)
+    console.log(movieData)
+  }
+
+  useEffect(() => {
+    fetchData()
+  })
+
+  return <div className="container">hej</div>
 }
 
-export default App;
+export default App
